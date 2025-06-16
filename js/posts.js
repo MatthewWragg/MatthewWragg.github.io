f38b378b36949f14e2d9c5c282882b63cc392562
MatthewWragg <matthew.wragg@bristol.ac.uk>
2025-06-16 17:04:36 +0100

    1

--- js/posts.js
+++ js/posts.js
@@ -1,12 +1,12 @@
 const postsData = [
   {
-    id: 1,
+    id: "intro",
     title: "First post and introduction",
     date: "2025-06-13T10:30:00Z",
     category: "General",
-    excerpt: "Hello! After a long time in the making, I am delighted to introduce my new blog! I hope to share research findings, personal updates, events, and lots of other research-related things. My main goal with this blog is to help make the research process as accessible and inclusive as possible, and to give some insights into the world of digital health research.",
-    content: `<p>Hello! After a long time in the making, I am delighted to introduce my new blog! I hope to share research findings, personal updates, events, and lots of other research-related things. My main goal with this blog is to help make the research process as accessible and inclusive as possible, and to give some insights into the world of digital health research.</p>
-    <p>I've had a few ideas for some regular posts, including updates from my fellowship with the Royal National Hospital for Rheumatic Diseases, opportunities to take part in research studies, and events organised by LEAP.</p>
-    <p>I would also love to create posts sharing your personal stories, particularly around taking part in research and accessing research findings. If you have anything you would like to share or any topics you would like me to cover, please get in touch!</p>`
+    excerpt: "Hello! After a long time in the making, I am delighted to introduce my new blog! I hope to share research findings, personal updates, events, and lots of other research-related things. One of my main goals with this blog is to help make research as accessible and inclusive as possible, particularly by sharing the outputs and impacts of our research, and by supporting patient and public involvement and engagement throughout every stage of research",
+    content: `<p>Hello! After a long time in the making, I am delighted to introduce my new blog! I hope to share research findings, personal updates, events, and lots of other research-related things. One of my main goals with this blog is to help make research as accessible and inclusive as possible, particularly by sharing research findings and supporting patient and public involvement and engagement.</p>
+    <p>I've had a few ideas for some regular posts, including updates from my fellowship work with the Royal National Hospital for Rheumatic Diseases, opportunities to take part in research studies, and events organised by LEAP.</p>
+    <p>I would also love to create posts sharing your personal stories, particularly around taking part in research and accessing research findings. If you have anything you would like to share or any other topics you would like me to cover, please get in touch!</p>`
   },
 ];
