f01a875b1b68ba326b6766e7185d1e239785eff5
MatthewWragg <matthew.wragg@bristol.ac.uk>
2025-07-18 16:58:35 +0100

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,6 +1,6 @@
 const postsData = [
   {
-    id: "intro",
+    id: "0",
     title: "First post and introduction",
     date: "2025-06-13T10:30:00Z",
     category: "General",
@@ -8,5 +8,33 @@ const postsData = [
     content: `<p>Hello! After a long time in the making, I am delighted to introduce my new blog! I hope to share research findings, personal updates, events, and lots of other research-related things. One of my main goals with this blog is to help make research as accessible and inclusive as possible, particularly by sharing research findings and supporting patient and public involvement and engagement.</p>
     <p>I've had a few ideas for some regular posts, including updates from my fellowship work with the Royal National Hospital for Rheumatic Diseases, opportunities to take part in research studies, and events organised by LEAP.</p>
     <p>I would also love to create posts sharing your personal stories, particularly around taking part in research and accessing research findings. If you have anything you would like to share or any other topics you would like me to cover, please get in touch!</p>`
+  },
+
+  {
+    id: "1",
+    title: "Patient Engagement Event and Research Updates",
+    date: "2025-07-17T15:00:00Z",
+    category: "axSpA",
+    excerpt: "Hello! After a long time in the making, I am delighted to introduce my new blog! I hope to share research findings, personal updates, events, and lots of other research-related things. One of my main goals with this blog is to help make research as accessible and inclusive as possible, particularly by sharing the outputs and impacts of our research, and by supporting patient and public involvement and engagement throughout every stage of research",
+    content: `<p>Hi everyone,<br><br>
+    I thought I would share some exciting updates from my axSpA research project over the last couple of weeks. Hope you enjoy!<br>
+
+    <h3>Patient engagement event</h3>
+    
+    
+    Last weekend, we ran another research patient engagement event at the University of Bath. It was a chance for people living with axSpA to meet with academic researchers and clinicians, to discuss current and future research at the RUH, the University of Bath, and the wider SPARC group. Despite the heatwave, we had around 40 attendees which was fantastic!</p>
+    <p>We started the day with an insightful panel discussion with two people living with axSpA, who talked about their diagnosis journeys, self-management practices, and how axSpA has affected their lives. We then presented the current research projects happening at the university, and invited attendees to take part in a series of workshops run by the researchers. I had the pleasure of running two of these workshops based on my fellowship research, looking at what information people record about their health outside of the clinic, and how this could be used during appointments to help guide shared decision-making and personalised care.</p>
+
+    <p>A huge thank you to those that took part in any of the workshops. I know I speak for all of the other researchers when I say that discussions like those are absolutely invaluable for us, and are crucial for designing research that reflects the needs and perspectives of people with lived experience. If you attended these sessions, I hope they were useful for you and gave you some insights into the research happening at the RUH and the university.</p>
+
+    <p>We will be running more of these events in the future, so if you're considering attending - please do!</p>
+    
+    <h3>RUH Study</h3>
+
+    <p>I'm delighted to share that we have received favourable ethical opinion from the NHS ethics committee for our study at the RUH! We will be recruiting participants through the RUH until the end of October 2025, so if you have an appointment at the RUH for your axSpA care during this time, a member of your care team may be in touch to discuss whether you might like to take part.</p>
+
+    <p>Another huge thank you to the BIRD patient panel who provided lots of insightful feedback on our study plans and documents. Being a part of a research panel is a great way to get involved in research without having to participate in studies, and helps us to design more accessible, inclusive, and meaningful research. If you would like to know more about participating in the BIRD patient panel, please get in touch with Mel at <a href="mailto:ppe@birdbath.org.uk">ppe@birdbath.org.uk</a>.</p>
+    
+    `
   },
 ];
