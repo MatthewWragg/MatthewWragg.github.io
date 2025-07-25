911476d447f3a7fe5dce0e5f014cf00db637ff5a
MatthewWragg <matthew.wragg@bristol.ac.uk>
2025-07-25 14:30:21 +0100

    blog and fellowship

--- js/posts.js
+++ js/posts.js
@@ -21,13 +21,23 @@ const postsData = [
 
     <h3>Patient engagement event</h3>
     
-    Last weekend, we ran another research patient engagement event at the University of Bath. It was a chance for people living with axSpA to meet with academic researchers and clinicians, to discuss current and future research at the RUH, the University of Bath, and the wider SPARC group. Despite the heatwave, we had around 40 attendees which was fantastic!</p>
+    Last weekend, we ran another research patient engagement event at the University of Bath. It was a chance for people living with axSpA to meet with academic researchers and clinicians, to discuss current and future research at university, and to explore ways to get involved with and guide research studies. Despite the heatwave, we had around 40 attendees which was fantastic!</p>
+
     <p>We started the day with an insightful panel discussion with two people living with axSpA, who talked about their diagnosis journeys, self-management practices, and how axSpA has affected their lives. We then presented the current research projects happening at the university, and invited attendees to take part in a series of workshops run by the researchers. I had the pleasure of running two of these workshops based on my fellowship research, looking at what information people record about their health outside of the clinic, and how this could be used during appointments to help guide shared decision-making and personalised care.</p>
 
-    <p>A huge thank you to those that took part in any of the workshops. I know I speak for all of the other researchers when I say that discussions like those are absolutely invaluable for us, and are crucial for designing research that reflects the needs and perspectives of people with lived experience. If you attended these sessions, I hope they were useful for you and gave you some insights into the research happening at the RUH and the university.</p>
-
-    <p>We will be running more of these events in the future, so if you're considering attending - please do!</p>
+    <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(10%, 49%)); gap: 1rem; margin-bottom: 40px; margin-top: 40px;">
+      <img src="./images/patientday1.jpg" style="margin: 0px;">
+      <img src="./images/patientday2.jpg" style="margin: 0px;">
+      <img src="./images/patientday3.jpg" style="margin: 0px;">
+      <img src="./images/patientday5.jpg" style="margin: 0px;">
+    </div>
     
+    <p>A huge thank you to those that took part in any of the workshops, and to all of the organisers and researchers. I know I speak for all of the other researchers when I say that discussions like those are absolutely invaluable for us, and are crucial for designing research that reflects the needs and perspectives of people with lived experience. If you attended these sessions, I hope they were useful for you and gave you some insights into the research happening at the RUH and the university. We will be running more of these events in the future, so if you're considering attending - please do!</p>
+
+    <div style="margin-top: 40px;">
+      <img src="./images/patientday4.jpg" style="margin: 0px;">
+    </div>
+
     <h3>axSpA research at the RUH</h3>
 
     <p>I'm delighted to share that we have received favourable ethical opinion from the NHS ethics committee for our study at the RUH! We will be recruiting participants through the RUH until the end of October 2025, so if you have an appointment at the RUH for your axSpA care during this time, a member of your care team may be in touch to discuss whether you might like to take part. We have also submitted an application for NIHR Research for Patient Benefit (RfPB) funding, which would allow us to continue our axSpA research with the RUH for another 2.5 years.</p>
